4fb455728009f6256ea13b7b5e73e7697a1f2b90
Maciej Glownia <maciej.glownia@infor.com>
2023-02-24 08:00:53 +0100

    Initial commit

---
+++ src/test/java/com/glownia/maciej/stoicsquotesquizquestionsrestapi/StoicsQuotesQuizQuestionsRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glownia.maciej.stoicsquotesquizquestionsrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoicsQuotesQuizQuestionsRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
